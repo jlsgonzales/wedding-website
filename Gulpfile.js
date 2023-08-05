1489c3ab2f4f769e378418e7cec09782a20c6524
Jolo Gonzales <jolosygonzales@outlook.com>
2023-08-05 15:25:00 +0800

    Initial Working Commit

---
+++ Gulpfile.js
@@ -0,0 +1,151 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+var cp          = require('child_process');
+var sourcemaps = require('gulp-sourcemaps');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
+var sourcemaps = require('gulp-sourcemaps');
+var imagemin = require('gulp-imagemin');
+
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+/**
+ * Build the Jekyll Site
+ */
+ gulp.task('jekyll-develop', function (done) {
+     browserSync.notify(messages.jekyllBuild);
+     return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+         .on('close', done);
+ });
+
+gulp.task('jekyll-build', function (done) {
+    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+
+
+gulp.task('browsersync-reload', (done) => {
+  browserSync.reload();
+  done()
+})
+
+gulp.task('browsersync-server', (done) => {
+  browserSync({
+    server: {
+        baseDir: '_site'
+    }
+  });
+  done();
+});
+
+
+
+/**
+ * Compile files from _scss
+ */
+gulp.task('sass-develop', function () {
+    return gulp.src('_sass/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths: ['scss'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 3 versions'], { cascade: true }))
+        .pipe(sourcemaps.write('maps'))
+        .pipe(gulp.dest('_site/css'))
+        .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('sass-build', function () {
+    return gulp.src('_sass/main.scss')
+        .pipe(sass({
+            includePaths: ['scss']
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('_site/css'));
+});
+
+/**
+ * Compile files from _js
+ */
+gulp.task('js-develop', function (done) {
+  var bundler = browserify({
+    entries: '_js-es6/app.js',
+    debug: true
+  });
+  bundler.transform(babelify);
+
+  bundler.bundle()
+    .on('error', function (err) { console.error(err); })
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('_site/js'))
+    .pipe(browserSync.reload({stream:true}));
+  done();
+});
+
+gulp.task('js-build', function (done) {
+  var bundler = browserify({
+    entries: '_js-es6/app.js',
+    debug: true
+  });
+  bundler.transform(babelify);
+
+  bundler.bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('_site/js'))
+  done()
+});
+
+/**
+ * Copy and compress any site images
+ */
+ gulp.task('copy-images', () =>
+     gulp.src('images/**/*.{jpg,png,svg}')
+         .pipe(imagemin())
+         .pipe(gulp.dest('_site/images'))
+ );
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function (done) {
+  gulp.watch(['_js-es6/**/*.js'], gulp.series(['js-develop']));
+  gulp.watch(['_sass/**/*.scss'], gulp.series(['sass-develop']));
+  gulp.watch(['**/*.html', '**/*.markdown', '**/*.md'], gulp.series(['jekyll-rebuild']));
+  done();
+});
+
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('jekyll-rebuild', gulp.series(['jekyll-develop', 'browsersync-reload']));
+
+/**
+ * Wait for jekyll-build, then launch the Server
+ */
+gulp.task('browser-sync', gulp.series(['sass-develop', 'js-develop', 'jekyll-develop', 'browsersync-server']));
+
+
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', gulp.series(['browser-sync', 'watch']));
+
+/**
+ * Gulp 'build' task which is used to build the site on the production box.
+ */
+gulp.task('build', gulp.series(['jekyll-build', 'sass-build', 'js-build']));
---
+++ README.md
@@ -0,0 +1,23 @@
+# barryandgillian.wedding
+
+This is the repo for our wedding website, which took place on 29 Sept 2016.
+
+## Getting started
+
+`yarn i`
+
+...will install all local dependencies.
+
+To fire up a local server while watching the site for changes just run;
+
+`yarn serve`
+
+...your browser of choice should open a new window and run the site at;
+
+`http://localhost:3000`
+
+## Running tests locally
+
+To run test locally, simply run;
+
+`yarn test`
